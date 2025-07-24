7c7b77cd67c1566a2353d773fc948d94c3a72ec2
uabdirasul <ubbiniyazov04@gmail.com>
2025-07-24 19:49:48 +0500

    edit texts & change sizes

--- src/features/auth/pages/sign-in/SignIn.tsx
+++ src/features/auth/pages/sign-in/SignIn.tsx
@@ -1,58 +1,84 @@
-import { Button, Form, Input } from "antd"
-import { useTranslation } from "react-i18next"
-import { Link, useNavigate } from "react-router-dom"
+import { Button, Form, Input } from "antd";
+import { useTranslation } from "react-i18next";
+import { Link, useNavigate } from "react-router-dom";
 
-import { useAppDispatch } from "@hooks/useRedux.ts"
-import { setIsAuth, setPermissions, setUser } from "@slices/userSlice"
-import { rootPaths } from "@utils/constants/rootPaths"
-import { currentUserToUser } from "@utils/helpers/currentUserToUser"
-import { useSignIn } from "../../services/authMutations"
-import { SignInBodyModel } from "../../utils/models/signInBodyModel"
+import { useAppDispatch } from "@hooks/useRedux.ts";
+import { setIsAuth, setPermissions, setUser } from "@slices/userSlice";
+import { rootPaths } from "@utils/constants/rootPaths";
+import { currentUserToUser } from "@utils/helpers/currentUserToUser";
+import { useSignIn } from "../../services/authMutations";
+import { SignInBodyModel } from "../../utils/models/signInBodyModel";
 
-import styles from "./signIn.module.scss"
+import styles from "./signIn.module.scss";
 
-const { Item } = Form
+const { Item } = Form;
 
 export const SignIn = () => {
-  const { t } = useTranslation('auth')
-  const [formInstance] = Form.useForm()
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
-  const signIn = useSignIn()
+  const { t } = useTranslation("auth");
+  const [formInstance] = Form.useForm();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const signIn = useSignIn();
 
   const onFinish = (fields: SignInBodyModel) => {
     signIn.mutateAsync(fields).then((res) => {
       if (res.accept) {
-        dispatch(setIsAuth(true))
-        dispatch(setPermissions(res.data.permissions))
-        dispatch(setUser(currentUserToUser(res.data)))
-        navigate(rootPaths.MAIN.INDEX)
-        formInstance.resetFields()
+        dispatch(setIsAuth(true));
+        dispatch(setPermissions(res.data.permissions));
+        dispatch(setUser(currentUserToUser(res.data)));
+        navigate(rootPaths.MAIN.INDEX);
+        formInstance.resetFields();
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className={styles.sign_in}>
       <div className={styles.header}></div>
       <div className={styles.body}>
         <div className={styles.body__title}>
-          <h1>{t('signInSection.header')}</h1>
-          <p>{t('signInSection.welcomeMessage')}</p>
+          <h1>{t("signInSection.header")}</h1>
+          <p>{t("signInSection.welcomeMessage")}</p>
         </div>
-        <Form form={formInstance} layout='vertical' className={styles.body__form} onFinish={onFinish}>
-          <Item label={t('enterEmail.email')} rules={[{ required: true, message: "" }]} name='email'>
-            <Input size='large' />
+        <Form
+          form={formInstance}
+          layout="vertical"
+          className={styles.body__form}
+          onFinish={onFinish}
+        >
+          <Item
+            label={t("enterEmail.email")}
+            rules={[{ required: true, message: "" }]}
+            name="email"
+          >
+            <Input size="large" placeholder={t("authEmailPlaceholder")} />
           </Item>
-          <Item label={t('formFields.password')} rules={[{ required: true, message: "" }]} name='password'>
-            <Input.Password size='large' />
+          <Item
+            label={t("formFields.password")}
+            rules={[{ required: true, message: "" }]}
+            name="password"
+          >
+            <Input.Password
+              size="large"
+              placeholder={t("authPasswordPlaceholder")}
+            />
           </Item>
-          <Link to={rootPaths.AUTH.RESET_PASSWORD}>{t('signInSection.forgotPassword')}</Link>
-          <Button size='large' loading={signIn.isPending} type='primary' htmlType='submit'>
-            {t('signInSection.header')}
+          <Link to={rootPaths.AUTH.RESET_PASSWORD}>
+            {t("signInSection.forgotPassword")}
+          </Link>
+          <Button
+            size="large"
+            loading={signIn.isPending}
+            type="primary"
+            htmlType="submit"
+          >
+            {t("signInSection.button-text")}
           </Button>
           <div className={styles.body__sign_up}>
-            {t('signInSection.noAccountPrompt')} <Link to={rootPaths.AUTH.SIGN_UP}>{t('signInSection.createAccount')}</Link>
+            {t("signInSection.noAccountPrompt")}{" "}
+            <Link to={rootPaths.AUTH.SIGN_UP}>
+              {t("signInSection.createAccount")}
+            </Link>
           </div>
         </Form>
       </div>
@@ -60,5 +86,5 @@ export const SignIn = () => {
         <p>Hi Solution® - 2025</p>
       </div>
     </div>
-  )
-}
+  );
+};
